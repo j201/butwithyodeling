86c45aa4a29c8d5cc029bc9cd2ee5b87e8cb06b3
Alex <j201.alex@gmail.com>
2015-03-21 21:42:36 -0400

    quickfixes

--- slideshow.js
+++ slideshow.js
@@ -1,6 +1,6 @@
 //adapted from http://www.willmaster.com/library/features/background-image-slide-show.php
 
-var a = new Array(
+var a = [
 	"http://www.andermatt-swissalps.ch/fileadmin/user_upload/Location/Andermatt/Back_Andermatt.jpg",
 	"http://upload.wikimedia.org/wikipedia/commons/d/da/Lauterbrunnental_train.jpg",
 	"https://brandondarnell.files.wordpress.com/2012/12/swiss-alps-lauterbrunnen-hiking.jpg",
@@ -11,16 +11,26 @@ var a = new Array(
 	"http://thecambrianadelboden.com/images/backgrounds/background_4.jpg",
 	"http://dimitrijeostojic.com/blog/wp-content/uploads/2011/08/matterhorn_00B.jpg",
 	"http://i.imgur.com/oX956Oc.png"	
-); 
+]; 
 
-var switchMilliseconds = 3000;
-var elemID = 'bg';
+var preloadImage;
+
+var switchMilliseconds = 5000;
+var bg = document.getElementById('bg');
 
 function publishPicture(i) {
-	document.getElementById(elemID).src = a[i]; 
-	i++;
-	if( i > (a.length - 1) ) { i = 0; }
-	setTimeout('publishPicture('+i+')',switchMilliseconds);
+	bg.src = a[i]; 
+	bg.style.minHeight = Math.max(700, window.innerHeight) + 'px';
+	bg.style.opacity = 1;
+
+	// preload next image
+	preloadImage = new Image();
+	preloadImage.src = a[(i + 1) % a.length];
+	
+	setTimeout(function() {
+		bg.style.opacity = 0;
+		setTimeout(publishPicture.bind(null, (i + 1) % a.length), 400);
+	}, switchMilliseconds);
 }
 publishPicture(0);
 
